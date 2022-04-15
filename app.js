63357d7d1f57f31b91c1b272eb952700af8f40ed
Tiagomt <tiago-tmt@hotmail.com>
2022-04-15 20:52:16 -0300

    avançando no JS e começo do style

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//criando cartas
 const cardArray = [
     {
         name: 'fries',
@@ -50,3 +51,26 @@ const cardArray = [
 
 ]
 
+//macete para misturar randomicamente um array
+cardArray.sort(() => 0.5 - Math.random())
+
+// selecionando div onde serao dipostos os cards
+const grid = document.querySelector("#grid")
+
+function createBoard() {
+
+    for (let i = 0; i < cardArray.length; i++) {
+
+        //criando tags img para imagens do game
+        const card = document.createElement("img")
+        //criando atributo src para a tag img e atribuindo valor a ele
+        card.setAttribute('src', './images/blank.png')
+        card.setAttribute('data-id', i)
+
+        //adicionando card(img) no final da grid(div)
+        grid.append(card)
+    }
+
+}
+
+createBoard()
